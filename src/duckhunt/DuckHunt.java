3964ff6e1a4c3ca0aaae6fa0747e417fcfde2d01
Rikki Guy <rikki.guy@gmx.com>
2013-02-07 15:57:10 +0000

    Initial Commit

---
+++ src/duckhunt/DuckHunt.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duckhunt;
+
+public class DuckHunt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("It Works!");
+    }
+}
