409c4ca9b2f96b9501f535ba8caadf9d3bf95e30
Rikki Guy <rikki.guy@gmx.com>
2013-03-01 11:36:18 +0000

    Finally at a state-based architecture

---
+++ src/duckhunt/ScoreState.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duckhunt;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author rikki
+ */
+public class ScoreState extends BasicGameState {
+    
+    private int stateId = -1;
+    
+    public ScoreState(int id)
+    {
+        this.stateId = id;
+    }
+
+    @Override public int getID()
+    {
+        return this.stateId;
+    }
+
+    @Override public void init(GameContainer gc, StateBasedGame sbg) throws SlickException
+    {
+        //init that thing
+    }
+
+    @Override public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException
+    {
+        //render stuff
+    }
+
+    @Override public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException
+    {
+        //update stuff
+    }
+   
+}
